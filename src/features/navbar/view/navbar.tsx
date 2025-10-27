d58d0ea61e1b93826e7b1ebd07307f9ffe578d4e
Долбеков Элдияр <eldiyar@MacBook-Pro-Dolbekov.local>
2025-10-27 19:49:46 +0600

    feat: navbar

--- src/features/navbar/view/navbar.tsx
+++ src/features/navbar/view/navbar.tsx
@@ -1,11 +1,33 @@
 import {NAV_LINKS} from "@/shared/lib/constants";
 import Link from "next/link";
+import Image from "next/image";
+import {useGSAP} from "@gsap/react";
+
 
 export function Navbar() {
+    useGSAP(() => {
+       const navTween = gsap.timeline({
+           scrollTrigger: {
+               trigger: "nav",
+               start: "bottom top",
+           }
+       });
+
+        navTween.fromTo("nav", { backgroundColor: "transparent" }, {
+            backgroundColor: "#00000050",
+            backgroundFilter: "blur(10px)",
+            ease: "power1.inOut",
+            duration: 1,
+        });
+    });
+
     return (
         <nav>
             <div className="flex gap-4">
-                <Link href="/">Home</Link>
+                <Link href="/" className='flex items-center gap-4'>
+                    <Image src='/images/logo.png' alt='Logo' width={32} height={32} />
+                    <p>Mojito</p>
+                </Link>
 
                 <ul className='flex gap-4'>
                     {NAV_LINKS.map((link) => (
